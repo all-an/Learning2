567ce28afff26227a933912c54c4d70ac7ef93ef
all-an <all-an@hotmail.com>
2020-07-18 17:26:58 -0300

    learning

---
+++ class2basicsLinkedin/code1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*int main()
+{
+    /*char x,y,z;
+
+    x= 'A';
+    y= 'B';
+    z= 'C';
+
+    printf("It's as easy as %c%c%c\n",x,y,z);
+    
+    int age = 30;
+    printf("The C language is over %d years old !\n",age);
+    age = 35;
+    printf("The C language is over %d years old !\n\n",age);
+    return(0);
+}*/
+
+int main()
+{
+    float pi;
+
+    pi = 22.00/7.0;
+    printf("The ancients calculated PI as %f.\n", pi);
+
+    return(0);
+}
