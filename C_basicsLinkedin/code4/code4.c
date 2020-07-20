3cb6f2d49952f9a158c0d074b42dae0fc2868edc
all-an <all-an@hotmail.com>
2020-07-20 09:54:39 -0300

    learning

---
+++ C_basicsLinkedin/code4/code4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char a,b,c;
+
+    printf("Type three letters: ");
+    a = getchar();
+    putchar(a);
+    b = getchar();
+    putchar(b);
+    c = getchar();
+    putchar(c);
+    //printf("\n%c, %c, %c.\n",a,b,c);
+
+    return(0);
+
+}
