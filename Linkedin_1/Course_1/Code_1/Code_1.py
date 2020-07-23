749994cc5e63a4be92a7bdcd12bec9dc6e3106ef
all-an <all-an@hotmail.com>
2020-07-23 08:57:33 -0300

    learning

--- Linkedin_1/Course_1/Code_1/Code_1.py
+++ Linkedin_1/Course_1/Code_1/Code_1.py
@@ -11,6 +11,32 @@ print("Hi " * 9)
 
 age = 36
 print(age)
+print(type(age))
 
 email_adress = "john.doe@example.com"
 print(email_adress)
+print(type(email_adress))
+
+
+import keyword
+
+print(keyword.kwlist)
+
+print(2 ** 3)
+
+print(3 / 2)
+
+print(3 // 2)
+
+print(5 % 3)
+
+print(45000 / 5)
+
+print(0.2 + 0.3)
+
+print(0.2 / 0.3)
+
+print("Hi there!")
+
+message = "Hi there!"
+print(message)
---
+++ Linkedin_1/Course_1/Code_2/Code_2.py
@@ -0,0 +1,10 @@
+print("Hi!")
+name = input("What's your name? ")
+
+print("It's nice to meet you, ", name)
+answer = input("Are you enjoying the course ? (Yes or No) ")
+
+if answer == "Yes":
+    print("Thats good to heare!")
+else:
+    print("Oh no! That makes me sad!")
